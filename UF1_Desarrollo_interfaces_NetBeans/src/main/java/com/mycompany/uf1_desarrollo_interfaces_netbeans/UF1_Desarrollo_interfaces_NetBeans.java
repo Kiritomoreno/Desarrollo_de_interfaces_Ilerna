88bc5d37ba4530242c5451552524c566a7254b19
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-09-28 18:31:24 +0100

    Desarrollo de los ejercicios con el IDE NetBeans

---
+++ UF1_Desarrollo_interfaces_NetBeans/src/main/java/com/mycompany/uf1_desarrollo_interfaces_netbeans/UF1_Desarrollo_interfaces_NetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uf1_desarrollo_interfaces_netbeans;
+
+/**
+ *
+ * @author KHIRANN
+ */
+public class UF1_Desarrollo_interfaces_NetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
